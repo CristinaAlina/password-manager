55b32b6b0267e64a8b733c8e17ba1f6b9233dad2
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2024-01-02 00:15:18 +0200

    Added save information command from all the entries into a local file

--- main.py
+++ main.py
@@ -4,7 +4,27 @@ RED = "#7D0A0A"
 LIGHT_YELLOW = "#F3EDC8"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def default_interface():
+    """Clears all the entries and focus on website entry field"""
+    website_entry.delete(0, END)
+    username_entry.delete(0, username_entry.get().find("@"))
+    password_entry.delete(0, END)
+    website_entry.focus()
+
+
+def save_information():
+    """Saves all the entries data into a local file"""
+    # Get the text from all entries
+    website_text = website_entry.get()
+    username_text = username_entry.get()
+    password_text = password_entry.get()
+    # Append the data into a local file
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website_text} | {username_text} | {password_text}\n")
+    default_interface()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window settings
@@ -17,7 +37,6 @@ canvas = Canvas(width=200, height=200, highlightthickness=0, bg="white")
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
 canvas.grid(column=0, row=0, columnspan=3)
-
 
 # Labels
 website_label = Label(text="Website:", bg="white", font=FONT, fg=RED)
@@ -33,8 +52,8 @@ empty_label.grid(column=0, row=4, columnspan=4)
 # Entries
 website_entry = Entry(width=52, highlightthickness=1, border=1, font=FONT)
 website_entry.grid(column=1, row=1, columnspan=2)
-website_entry.focus()
 username_entry = Entry(width=52, highlightthickness=1, border=1, font=FONT)
+username_entry.insert(0, "@gmail.com")
 username_entry.grid(column=1, row=2, columnspan=2)
 password_entry = Entry(width=32, highlightthickness=1, border=1, font=FONT)
 password_entry.grid(column=1, row=3)
@@ -44,7 +63,9 @@ gen_pass_button = Button(text="Generate Password", width=16, font=FONT, bg=LIGHT
                          border=1, fg=RED)
 gen_pass_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=45, font=FONT, bg=LIGHT_YELLOW, highlightthickness=0,
-                    border=1, fg=RED)
+                    border=1, fg=RED, command=save_information)
 add_button.grid(column=1, row=5, columnspan=2)
+
+default_interface()
 
 window.mainloop()
